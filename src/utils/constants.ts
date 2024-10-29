733fe3c0846d6c101c293ae8771f8e73ab857171
Giampiero Ferrara <giampiero.ferrara@emeal.nttdata.com>
2024-10-29 16:28:09 +0100

    Added const for verify infocamere

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -3,5 +3,8 @@ import { ENV } from "../env";
 export const API = {
     IMPORT_PSP: {
         URL: ENV.URL_API.EXTERNAL + '/onboarding/psp/import',
+    },
+    VERIFY_INFOCAMERE: {
+        URL: ENV.URL_API.EXTERNAL + '/infocamere-pdnd/institution/',
     }
 };
