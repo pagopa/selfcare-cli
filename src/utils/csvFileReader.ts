f46c78762eaed38039c352a78d861e3f62ce1c0c
Giampiero Ferrara <giampiero.ferrara@emeal.nttdata.com>
2024-10-31 10:38:00 +0100

    Fixed parser for csv

--- src/utils/csvFileReader.ts
+++ src/utils/csvFileReader.ts
@@ -1,33 +1,39 @@
-import * as fs from 'fs';
-import * as path from 'path';
-import csvParser from 'csv-parser';
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
+import * as fs from "fs";
+import * as path from "path";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+import { parse } from "csv-parse";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 export const importCsvFile = async () => {
+  const csvFilePath = path.join(__dirname, "data", "../../utils/contracts.csv");
 
-    const csvFilePath = path.join(__dirname, 'data', '../../utils/contracts.csv');
+  const results: Array<any> = [];
 
-    const results: Array<any> = [];
+  const parser = parse({
+    delimiter: [";", "\t", ","],
+    columns: true,
+    skip_empty_lines: true,
+    trim: true,
+    bom: true,
+    relax_column_count: true
+  });
 
-    return new Promise<Array<any>>((resolve, reject) => {
-        fs.createReadStream(csvFilePath)
-            .pipe(csvParser({
-                separator: ';'
-            }))
-            .on('data', (data) => {
-                results.push(data)
-            })
-            .on('end', () => {
-                resolve(results);
-            })
-            .on('error', (error) => {
-                reject(error);
-            });
-    });
+  return new Promise<Array<any>>((resolve, reject) => {
+    fs.createReadStream(csvFilePath)
+      .pipe(parser)
+      .on("data", (data) => {
+        results.push(data);
+      })
+      .on("end", () => {
+        resolve(results);
+      })
+      .on("error", (error) => {
+        reject(error);
+      });
+  });
 };
 
 importCsvFile().catch(console.error);
