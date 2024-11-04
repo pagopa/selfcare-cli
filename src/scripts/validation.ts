7e9d6875c9b16dea7e27216ae2e3186abd9021f6
Giampiero Ferrara <giampiero.ferrara@emeal.nttdata.com>
2024-11-04 12:39:15 +0100

    Refactored validation

--- src/scripts/validation.ts
+++ src/scripts/validation.ts
@@ -11,65 +11,63 @@ export const Validation = async () => {
   );
 
   const results: Array<ContractOutput> = [];
-
-  let esito = false;
   const errors: Array<ContractOutput> = [];
 
-  contracts.forEach((contract) => {
-    const tax_code = contract.tax_code;
+  for (const contract of contracts) {
+    await new Promise((resolve) => setTimeout(resolve, 1000));
 
-    // call api
-    pspVerifyService(tax_code)
-      .then((res: any) => {
-        if (res.digitalAddress === undefined) {
-          contract.infocamere_pec = "NON PRESENTE SU INFOCAMERE";
-        } else {
-          contract.infocamere_pec = res.digitalAddress;
-        }
-        contract.zip_code = res.zipCode;
-        contract.county = res.county;
-        contract.city = res.city;
-        contract.country = "IT";
-        contract.registered_office = res.address;
-        contract.infocamere_name = res.businessName;
-        contract.institution_type = "PSP";
-        contract.product_id = "prod-pagopa";
-        contract.business_register_number = tax_code;
-        esito = true;
-      })
-      .catch((err) => {
-        console.log(`Errore per il codice fiscale ${tax_code}: ${err}`);
-        contract.infocamere_name = "ERRORE 404";
-        contract.infocamere_pec = "ERRORE 404";
-        contract.country = "-";
-        esito = false;
-        errors.push(pspOutputMapper(contract));
+    let tax_code = contract.tax_code;
+    let esito = false;
+
+    try {
+      // chiamata API
+      const res: any = await pspVerifyService(tax_code);
+
+      if (res.digitalAddress === undefined) {
+        contract.infocamere_pec = "NON PRESENTE SU INFOCAMERE";
+      } else {
+        contract.infocamere_pec = res.digitalAddress;
+      }
+
+      contract.zip_code = res.zipCode;
+      contract.county = res.county;
+      contract.city = res.city;
+      contract.country = "IT";
+      contract.registered_office = res.address;
+      contract.infocamere_name = res.businessName;
+      contract.institution_type = "PSP";
+      contract.product_id = "prod-pagopa";
+      contract.business_register_number = tax_code;
+      esito = true;
+      
+    } catch (err) {
+      console.log(`Errore per il codice fiscale ${tax_code}: ${err}`);
+      contract.infocamere_name = "ERRORE 404";
+      contract.infocamere_pec = "ERRORE 404";
+      contract.country = "-";
+      errors.push(pspOutputMapper(contract));
+    } finally {
+      if (esito) {
+        console.log(`Ente ${contract.tax_code} verificato`);
         results.push(pspOutputMapper(contract));
-      })
-      .finally(() => {
-        if (esito) {
-          console.log(`Ente ${contract.tax_code} verificato`);
-          results.push(pspOutputMapper(contract));
-        } else {
-          console.log("Non ci sono enti da validare");
-        }
+      } else {
+        console.log("Non ci sono enti da validare");
+      }
+    }
+  }
 
-        if (contracts_size === results.length) {
-          if (results.length != 0) {
-            csvFileWriter(results);
-          } else {
-            console.log("Non ci enti da validare");
-          }
+  if (results.length != 0) {
+    csvFileWriter(results);
+  } else {
+    console.log("Non ci enti da validare");
+  }
 
-          if (errors.length != 0) {
-            console.log(
-              "Enti in cui le chiamate vanno in eccezione",
-              errors.map((err) => `${err.tax_code} ${err.name}`)
-            );
-          } else {
-            console.log("Enti non in errore.");
-          }
-        }
-      });
-  });
+  if (errors.length != 0) {
+    console.log(
+      "Enti in cui le chiamate vanno in eccezione",
+      errors.map((err) => `${err.tax_code} ${err.name}`)
+    );
+  } else {
+    console.log("Enti non in errore.");
+  }
 };
